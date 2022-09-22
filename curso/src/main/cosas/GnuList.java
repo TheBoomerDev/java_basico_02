e3986bd08a4032b87a2997179197594f6702aa94
Jorge Parada <jorge.parada@ctbarbanza.com>
2022-09-22 11:49:49 +0200

    Reduce + Chorrada

--- curso/src/main/cosas/GnuList.java
+++ curso/src/main/cosas/GnuList.java
@@ -1,6 +1,7 @@
 package main.cosas;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -8,23 +9,29 @@ public class GnuList {
 
 	public static void main(String[] args) {
 		
-		String[] alphabet = "a b c d e f g h i j k l m n o p q r s t u v w x y z".split(" ");
+		List<String> letters = Arrays.asList("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
 		
-		List<String> alf = new ArrayList<String>();
-		for (String letter : alphabet) {
-			alf.add(letter);
-		}
-		
-		 Stream<Object> names = alf.stream().map((letter)->{
-			 String format = "%sNU %s Not Unix";
-			 letter = letter.toUpperCase();
-			 String msg = String.format(format, letter, letter);
-			return msg;
-		});
 		 
-		 for (Object name : names.toArray()) {
-			System.out.println(name);
-		}
+		String res = letters.stream().reduce("", (elementoPrev, current)->{
+			return elementoPrev+"-"+current.toUpperCase();
+		});
+		
+		System.out.println(res);
+		
+		
+		// ======= Sacar una media
+		
+		Integer resI = letters.stream().map((letter)->{
+			return letters.indexOf(letter);
+		}).reduce(1, (prev, current)->{
+			return (int)((prev + current));
+		});
+		
+		int size = letters.size();
+		String format = "REs: %d - Size: %d -> DIV: %s";
+		String msg = String.format(format, resI, size, (resI / size));
+		System.out.println(msg);
+		 
 		
 	}
 	
